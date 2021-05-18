20a390d1a55aaed2e2301ce9bf97dc99606297ba
sandeep <sandeepsandy2530@gmail.com>
2021-05-18 16:45:45 +1000

    Added Basic stylesheet.

--- src/index.php
+++ src/index.php
@@ -1,6 +1,20 @@
-<?php
 
-echo "Welcome to sorce Index page or Main Page";
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    
+    <title>visas.com.au</title>
+</head>
+<body>
+ <h1><center>Visas.com.au</center></h1>
+ 
+ <?php
+
+echo '<h2><center>Coming Soon...</center></h2>';
 
 
 ?>
+</body>
+</html>
--- src/staff/index.php
+++ src/staff/index.php
@@ -1,6 +1,26 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Visas.com.au</title>
+    <link rel="stylesheet" media="all" href="../stylesheets/staff.css" />
 
-echo "Hello World from staff page";
+</head>
+<body>
+    <header><h1><center>visas.com.au</center></h1></header>
+    <navigation>
+    
+    <ul>
+        <li><a href="index.php">Menu</a></li>
+    </ul>
+    </navigation>
 
+    <div id="content"></div>
 
-?>
+    <footer><center>
+    &copy; <?php echo date('Y'); ?> Visas.com.au
+    </center>
+    </footer>
+
+</body>
+</html>
