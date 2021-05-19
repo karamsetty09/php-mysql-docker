576203496fa2b5cd70446e18bb30e23f101188fd
sandeep <sandeepsandy2530@gmail.com>
2021-05-19 15:47:43 +1000

    Encode URL parameters using php funtions.

--- src/public/staff/visas/show.php
+++ src/public/staff/visas/show.php
@@ -1,5 +1,13 @@
+<?php require_once('../../../private/initialize.php'); ?>
+
 <?php
 echo "In show page";
-$id = $_GET['id'];
+$id = $_GET['id'] ?? '1';
 echo "Id Number:" . $id;
+
+
 ?>
+<br>
+<a href="show.php?name=<?php echo u('sandeep'); ?>">Click link to pass parameter of name</a><br>
+<a href="show.php?company=<?php echo u('Bannister&Law'); ?>">Click link to pass parameter of company</a><br>
+<a href="show.php?Query=<?php echo u('!@#$*'); ?>">Click link to pass parameter of query</a>
