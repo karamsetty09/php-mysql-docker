6440727d0d0fc241160b0d508b860f80119595bb
sandeep <sandeepsandy2530@gmail.com>
2021-05-24 12:01:32 +1000

    Handling errors of Headers, page redirection and output buffering.

--- src/private/initialize.php
+++ src/private/initialize.php
@@ -1,5 +1,8 @@
 <?php
 
+ob_start(); //output buffering is enabled.
+
+
 // Assign file paths to PHP constants
 // __FILE__ returns the current path to this file
 // direname() returns the path to the parent directory
