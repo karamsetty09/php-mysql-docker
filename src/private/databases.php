e9678beeb4930f12f8639d09d961d322685a20c5
karamsetty09 <sandeepsandy2530@gmail.com>
2021-05-26 12:47:46 +1000

    Error handling for database connection and queries.

--- src/private/databases.php
+++ src/private/databases.php
@@ -13,7 +13,22 @@ function db_connect(){
 function db_disconnect($connection){
     if(isset($connection)){
         mysqli_close($connection);
-    }
-    
+    } 
 }
+
+function confirm_db_connect(){
+    if(mysqli_connect_errno()) {
+        $msg = "Database connection failed: ";
+        $msg .= mysqli_connect_error();
+        $msg .= " (" . mysqli_connet_errno() . ")";
+        exit($msg); 
+    }
+}
+
+function confirm_result_set($result_set) {
+    if(!$result_set){
+        exit("Database query failed.");
+    }
+}
+
 ?>
--- src/private/query_functions.php
+++ src/private/query_functions.php
@@ -7,6 +7,7 @@ function find_all_visas(){
     $sql = "SELECT * FROM VISAS_TYPES ";
     $sql .= "ORDER BY position ASC";
     $result = mysqli_query($db, $sql);
+    confirm_result_set($result);
     return $result;
 }
 
